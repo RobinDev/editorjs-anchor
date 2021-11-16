e34e6546912c9343c3f8a339f983fd31fb5295ad
Robin@PiedWeb <contact@piedweb.com>
2021-11-16 11:47:40 +0100

    Store only the anchor instead of an object containing the anchor
    It's a BC Break suggestion permitting to have the data stored in  `tunes.anchor` instead of  `tunes.anchor.anchor`

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ class AnchorBlockTune {
      * @returns {string}
      */
     getAnchor(){
-        return this.data.anchor || '';
+        return this.data || '';
     }
-    
+
     /**
      * Constructor
      *
@@ -29,8 +29,8 @@ class AnchorBlockTune {
      */
     constructor({ api, data }) {
         this.api = api;
-        this.data = data || {};
-        
+        this.data = data || '';
+
         this._CSS = {
           classWrapper: 'cdx-anchor-tune-wrapper',
           classIcon: 'cdx-anchor-tune-icon',
@@ -45,11 +45,11 @@ class AnchorBlockTune {
     render() {
         const wrapper = document.createElement('div');
         wrapper.classList.add(this._CSS.classWrapper);
-        
+
         const wrapperIcon = document.createElement('div');
         wrapperIcon.classList.add(this._CSS.classIcon);
         wrapperIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" x="0" y="0" viewBox="0 0 32 32" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg"><g id="hash"><path style="" d="M30,12V8h-5.004l1-8h-4l-1,8h-7.998l1-8h-4l-1,8H2v4h6.498L7.5,20H2v4h5l-1,8h4l1-8h8l-1.002,8H22    l1-8h7v-4h-6.5l0.996-8H30z M19.5,20h-8l0.998-8h7.998L19.5,20z" fill="#878787" data-original="#030104" class=""/></g></g></svg>';
-        
+
         const wrapperInput = document.createElement('input');
         wrapperInput.placeholder = this.api.i18n.t('Anchor');
         wrapperInput.classList.add(this._CSS.classInput);
@@ -61,9 +61,9 @@ class AnchorBlockTune {
 
             // Save value
             if (value.length > 0) {
-                this.data.anchor = value;
+                this.data = value;
             } else {
-                this.data.anchor = undefined;
+                this.data = '';
             }
         });
 
